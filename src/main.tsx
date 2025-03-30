ca09bc9ebbd729cc73349eaef424fbcb853dde6c
cr-eative-dev / <github@mail.cr.eative.dev>
2025-03-30 11:38:36 +0200

    logger

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { getSmProvider } from 'polkadot-api/sm-provider'
 import { chainSpec } from 'polkadot-api/chains/westend2';
 import { start } from 'polkadot-api/smoldot';
 import { getInjectedExtensions, connectInjectedExtension } from 'polkadot-api/pjs-signer'
+import { withLogsRecorder } from 'polkadot-api/logs-provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -23,58 +24,60 @@ const chain = await smoldot.addChain({
 
 const provider = getSmProvider(chain);
 
-const client = createClient(provider);
+const loggedProvider = withLogsRecorder(console.log, provider)
+
+const client = createClient(loggedProvider);
 
 const typedApi = client.getTypedApi(wnd);
 
-// const token = await typedApi.compatibilityToken;
+const token = await typedApi.compatibilityToken;
 
-// const account = await typedApi.query.System.Account.getValue("5FxrUu1PUugUYs6HQ83bDswjGLyHYTEzm7yqmrkKVPaYe71Y")
+const account = await typedApi.query.System.Account.getValue("5FxrUu1PUugUYs6HQ83bDswjGLyHYTEzm7yqmrkKVPaYe71Y")
 
 
-// console.log(typedApi.constants.System.SS58Prefix(token));
+console.log(typedApi.constants.System.SS58Prefix(token));
 
 // const tx = typedApi.tx.System.remark({
 //   remark: Binary.fromText("Hello World")
 // })
 
-// const encodedData = await tx.getEncodedData();
+const encodedData = await tx.getEncodedData();
 
-// const result = await typedApi.apis.TransactionPaymentCallApi.query_call_info(
-//   tx.decodedCall,
-//   0
-// );
+const result = await typedApi.apis.TransactionPaymentCallApi.query_call_info(
+  tx.decodedCall,
+  0
+);
 
-// console.log(result);
+console.log(result);
 
-// const sudoAccount = await typedApi.query.Sudo.Key.getValue();
-// if (sudoAccount !== undefined) {
-//   console.log("Sudo acc:", sudoAccount.toString());
-// } else {
-//   console.log("Sudo acc is undefined");
-// }
+const sudoAccount = await typedApi.query.Sudo.Key.getValue();
+if (sudoAccount !== undefined) {
+  console.log("Sudo acc:", sudoAccount.toString());
+} else {
+  console.log("Sudo acc is undefined");
+}
 
-// if (sudoAccount !== undefined) {
-//   const sudoAccountInfo = await typedApi.query.System.Account.getValue(sudoAccount);
-//   console.log("Sudo acc balance:", sudoAccountInfo.data.free.toString());
-// } else {
-//   console.log("Sudo account is undefined, cannot fetch account info.");
-// }
+if (sudoAccount !== undefined) {
+  const sudoAccountInfo = await typedApi.query.System.Account.getValue(sudoAccount);
+  console.log("Sudo acc balance:", sudoAccountInfo.data.free.toString());
+} else {
+  console.log("Sudo account is undefined, cannot fetch account info.");
+}
 
-// const proxyEntries = await typedApi.query.Proxy.Proxies.getEntries();
+const proxyEntries = await typedApi.query.Proxy.Proxies.getEntries();
 
-// const anyProxies = proxyEntries.flatMap((entry) => entry.value[0].filter((v) => v.proxy_type.type === "Any")).map((v) => v.delegate);
+const anyProxies = proxyEntries.flatMap((entry) => entry.value[0].filter((v) => v.proxy_type.type === "Any")).map((v) => v.delegate);
 
-// const anyProxiesUnique = new Set(anyProxies)
+const anyProxiesUnique = new Set(anyProxies)
 
-// console.log("Any Proxies:", anyProxiesUnique);
+console.log("Any Proxies:", anyProxiesUnique);
 
-// const maxProxies = typedApi.constants.Proxy.MaxProxies()
+const maxProxies = typedApi.constants.Proxy.MaxProxies()
 
-// const maxProxiesValue = await maxProxies;
-// const thirdResult = proxyEntries.some((entry) => entry.value[0].length >= maxProxiesValue);
+const maxProxiesValue = await maxProxies;
+const thirdResult = proxyEntries.some((entry) => entry.value[0].length >= maxProxiesValue);
 
-// console.log("Third result:", thirdResult);
+console.log("Third result:", thirdResult);
 
 const extensionId = getInjectedExtensions()[0];
 const extension = await connectInjectedExtension(extensionId);
